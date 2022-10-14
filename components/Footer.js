bacbea01221cdc9d3b30da5161f2afd67e6c98d7
Andrii Levchuk <andriykolevchuk@gmail.com>
2022-10-14 12:04:07 +0200

    completed styliing and all main components

---
+++ components/Footer.js
@@ -0,0 +1,43 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+		<div className='grid grid-cols-1 md:grid-cols-4 gap-y-10 px-32 py-14 bg-gray-100 text-gray-600'>
+			<div className='space-y-4 text-xs text-gray-800'>
+				<h5 className='font-bold'>COMMUNITY</h5>
+				<p>Accessibility</p>
+				<p>This is not real site</p>
+				<p>Its a pretty awesome ALEVCHUK clone</p>
+				<p>Referrals accepted</p>
+				<p>ALEVCHUK</p>
+			</div>
+			<div className='space-y-4 text-xs text-gray-800'>
+				<h5 className='font-bold'>ABOUT</h5>
+				<p>How Airbnb works</p>
+				<p>Newsroom</p>
+				<p>Investors</p>
+				<p>Airbnb Plus</p>
+				<p>Airbnb Luxe</p>
+			</div>
+			<div className='space-y-4 text-xs text-gray-800'>
+				<h5 className='font-bold'>HOST</h5>
+				<p>ALEVCHUK skill mastering</p>
+				<p>Presents</p>
+				<p>From Zero to Full Stack Developer</p>
+				<p>Check my GitHub</p>
+				<p>Enjoy</p>
+			</div>
+			<div className='space-y-4 text-xs text-gray-800'>
+				<h5 className='font-bold'>SUPPORT</h5>
+				<p>Help Center</p>
+				<p>Trust & Safety</p>
+				<p>Say Hello To Me</p>
+				<p>Cool Website</p>
+				<p>For the Win</p>
+			</div>
+			<div></div>
+		</div>
+	)
+}
+
+export default Footer
